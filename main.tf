66f9a07498f542849b8e4c77bab0b88a815288d5
Uchinda Padmaperuma <uchinda@sph.com.sg>
2022-09-13 23:47:56 +0800

    adding the base of opensearch

---
+++ main.tf
@@ -0,0 +1,78 @@
+resource "aws_iam_service_linked_role" "opensearch" {
+  count            = var.create_service_role ? 1 : 0
+  aws_service_name = "opensearchservice.amazonaws.com"
+}
+
+resource "aws_opensearch_domain" "this" {
+  domain_name     = var.cluster_name
+  engine_version  = var.engine_version
+  access_policies = var.access_policies
+
+  cluster_config {
+    dedicated_master_enabled = var.master_instance_enabled
+    dedicated_master_count   = var.master_instance_enabled ? var.master_instance_count : null
+    dedicated_master_type    = var.master_instance_enabled ? var.master_instance_type : null
+
+    instance_count = var.instance_count
+    instance_type  = var.instance_type
+
+    warm_enabled = var.warm_instance_enabled
+    warm_count   = var.warm_instance_enabled ? var.warm_instance_count : null
+    warm_type    = var.warm_instance_enabled ? var.warm_instance_type : null
+
+    zone_awareness_enabled = (var.availability_zones > 1) ? true : false
+
+    dynamic "zone_awareness_config" {
+      for_each = (var.availability_zones > 1) ? [var.availability_zones] : []
+      content {
+        availability_zone_count = zone_awareness_config.value
+      }
+    }
+  }
+
+  vpc_options {
+    subnet_ids         = var.subnet_ids
+    security_group_ids = var.security_group_ids
+  }
+
+  advanced_security_options {
+    enabled                        = var.advanced_security_options_enabled
+    anonymous_auth_enabled         = var.anonymous_auth_enabled
+    internal_user_database_enabled = var.internal_user_database_enabled
+
+    master_user_options {
+      master_user_arn      = (!var.internal_user_database_enabled && var.master_user_arn != "") ? var.master_user_arn : data.aws_caller_identity.current.arn
+      master_user_name     = (var.internal_user_database_enabled && var.master_user_arn == "") ? var.master_user_name : null
+      master_user_password = (var.internal_user_database_enabled && var.master_user_arn == "") ? var.master_user_password : null
+    }
+  }
+
+  domain_endpoint_options {
+    enforce_https       = true
+    tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
+
+    custom_endpoint_enabled         = var.custom_endpoint_enabled
+    custom_endpoint                 = var.custom_endpoint_enabled ? var.custom_endpoint : null
+    custom_endpoint_certificate_arn = var.custom_endpoint_enabled ? var.custom_endpoint_certificate_arn : null
+  }
+
+  node_to_node_encryption {
+    enabled = true
+  }
+
+  encrypt_at_rest {
+    enabled    = true
+    kms_key_id = var.encrypt_kms_key_id
+  }
+
+  ebs_options {
+    ebs_enabled = var.ebs_enabled
+    volume_type = var.ebs_enabled ? var.ebs_volume_type : null
+    volume_size = var.ebs_enabled ? var.ebs_volume_size : null
+    iops        = can(regex("gp3|io1|io2", var.ebs_volume_type)) ? var.ebs_iops : null
+    throughput  = var.ebs_volume_type == "gp3" ? var.ebs_gp3_throughput : null
+  }
+  tags = var.tags
+
+  depends_on = [aws_iam_service_linked_role.opensearch]
+}
---
+++ variables.tf
@@ -0,0 +1,284 @@
+variable "cluster_name" {
+  description = "The name of the OpenSearch cluster."
+  type        = string
+  default     = "opensearch"
+}
+
+variable "cluster_version" {
+  description = "The version of OpenSearch to deploy."
+  type        = string
+  default     = "1.0"
+}
+
+variable "cluster_domain" {
+  description = "The hosted zone name of the OpenSearch cluster."
+  type        = string
+}
+
+variable "create_service_role" {
+  description = "Indicates whether to create the service-linked role. See https://docs.aws.amazon.com/opensearch-service/latest/developerguide/slr.html"
+  type        = bool
+  default     = true
+}
+
+variable "master_user_arn" {
+  description = "The ARN for the master user of the cluster. If not specified, then it defaults to using the IAM user that is making the request."
+  type        = string
+  default     = ""
+}
+
+variable "master_instance_enabled" {
+  description = "Indicates whether dedicated master nodes are enabled for the cluster."
+  type        = bool
+  default     = true
+}
+
+variable "master_instance_type" {
+  description = "The type of EC2 instances to run for each master node. A list of available instance types can you find at https://aws.amazon.com/en/opensearch-service/pricing/#On-Demand_instance_pricing"
+  type        = string
+  default     = "r6gd.large.elasticsearch"
+
+  validation {
+    condition     = can(regex("^[t3|m5|m6g|r5|r6g|r6gd|i3|c5|c6g]", var.master_instance_type))
+    error_message = "The EC2 master_instance_type must provide a SSD or NVMe-based local storage."
+  }
+}
+
+variable "master_instance_count" {
+  description = "The number of dedicated master nodes in the cluster."
+  type        = number
+  default     = 3
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instances to run for each hot node. A list of available instance types can you find at https://aws.amazon.com/en/opensearch-service/pricing/#On-Demand_instance_pricing"
+  type        = string
+  default     = "r6gd.4xlarge.elasticsearch"
+
+  validation {
+    condition     = can(regex("^[t3|m5|m6g|r5|r6g|r6gd|i3|c5|c6g]", var.instance_type))
+    error_message = "The EC2 instance_type must provide a SSD or NVMe-based local storage."
+  }
+}
+
+variable "instance_count" {
+  description = "The number of dedicated hot nodes in the cluster."
+  type        = number
+  default     = 3
+}
+
+variable "warm_instance_enabled" {
+  description = "Indicates whether ultrawarm nodes are enabled for the cluster."
+  type        = bool
+  default     = true
+}
+
+variable "warm_instance_type" {
+  description = "The type of EC2 instances to run for each warm node. A list of available instance types can you find at https://aws.amazon.com/en/elasticsearch-service/pricing/#UltraWarm_pricing"
+  type        = string
+  default     = "ultrawarm1.large.elasticsearch"
+}
+
+variable "warm_instance_count" {
+  description = "The number of dedicated warm nodes in the cluster."
+  type        = number
+  default     = 3
+}
+
+variable "availability_zones" {
+  description = "The number of availability zones for the OpenSearch cluster. Valid values: 1, 2 or 3."
+  type        = number
+  default     = 3
+}
+
+variable "encrypt_kms_key_id" {
+  description = "The KMS key ID to encrypt the OpenSearch cluster with. If not specified, then it defaults to using the AWS OpenSearch Service KMS key."
+  type        = string
+  default     = ""
+}
+
+variable "saml_subject_key" {
+  description = "Element of the SAML assertion to use for username."
+  type        = string
+  default     = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"
+}
+
+variable "saml_roles_key" {
+  description = "Element of the SAML assertion to use for backend roles."
+  type        = string
+  default     = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role"
+}
+
+variable "saml_entity_id" {
+  description = "The unique Entity ID of the application in SAML Identity Provider."
+  type        = string
+}
+
+variable "saml_metadata_content" {
+  description = "The metadata of the SAML application in xml format."
+  type        = string
+}
+
+variable "saml_session_timeout" {
+  description = "Duration of a session in minutes after a user logs in. Default is 60. Maximum value is 1,440."
+  type        = number
+  default     = 60
+}
+
+variable "saml_master_backend_role" {
+  description = "This backend role receives full permissions to the cluster, equivalent to a new master role, but can only use those permissions within Dashboards."
+  type        = string
+  default     = null
+}
+
+variable "saml_master_user_name" {
+  description = "This username receives full permissions to the cluster, equivalent to a new master user, but can only use those permissions within Dashboards."
+  type        = string
+  default     = null
+}
+
+variable "index_templates" {
+  description = "A map of all index templates to create."
+  type        = map(any)
+  default     = {}
+}
+
+variable "index_template_files" {
+  description = "A set of all index template files to create."
+  type        = set(string)
+  default     = []
+}
+
+variable "ism_policies" {
+  description = "A map of all ISM policies to create."
+  type        = map(any)
+  default     = {}
+}
+
+variable "ism_policy_files" {
+  description = "A set of all ISM policy files to create."
+  type        = set(string)
+  default     = []
+}
+
+variable "indices" {
+  description = "A map of all indices to create."
+  type        = map(any)
+  default     = {}
+}
+
+variable "index_files" {
+  description = "A set of all index files to create."
+  type        = set(string)
+  default     = []
+}
+
+variable "roles" {
+  description = "A map of all roles to create."
+  type        = map(any)
+  default     = {}
+}
+
+variable "role_files" {
+  description = "A set of all role files to create."
+  type        = set(string)
+  default     = []
+}
+
+variable "role_mappings" {
+  description = "A map of all role mappings to create."
+  type        = map(any)
+  default     = {}
+}
+
+variable "role_mapping_files" {
+  description = "A set of all role mapping files to create."
+  type        = set(string)
+  default     = []
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "ebs_enabled" {
+  description = "Whether EBS volumes are attached to data nodes in the domain"
+  type        = bool
+  default     = false
+}
+
+variable "ebs_volume_type" {
+  description = "Type of EBS volumes attached to data nodes"
+  type        = string
+  default     = "gp3"
+}
+
+variable "volume_size" {
+  description = "Size of EBS volumes attached to data nodes (in GiB)"
+  type        = number
+  default     = 10
+}
+
+variable "ebs_gp3_throughput" {
+  description = "Specifies the throughput (in MiB/s) of the EBS volumes attached to data nodes. Applicable only for the gp3 volume type. Valid values are between 125 and 1000"
+  type        = number
+  default     = 125
+
+  validation {
+    condition = (
+      var.ebs_gp3_throughput >= 125 &&
+      var.ebs_gp3_throughput <= 1000
+    )
+    error_message = "Must be between 125 and 1000 MiB/s"
+  }
+}
+
+variable "custom_endpoint_enabled" {
+  description = "custom endpoint enabled"
+  type        = bool
+  default     = false
+}
+
+variable "custom_endpoint" {
+  description = "Custom Endpoint URL"
+  type        = string
+  default     = null
+}
+
+variable "custom_endpoint_certificate_arn" {
+  description = "Custom Endpoint Certificate ARN"
+  type        = string
+  default     = null
+}
+
+variable "subnet_ids" {
+  description = "List of VPC Subnet IDs for the OpenSearch domain endpoints to be created in"
+  type        = list(string)
+  default     = []
+}
+
+variable "security_group_ids" {
+  description = "List of VPC Security Group IDs to be applied to the OpenSearch domain endpoints. If omitted, the default Security Group for the VPC will be used"
+  type        = list(string)
+  default     = []
+}
+
+variable "advanced_security_options_enabled" {
+  description = "Whether advanced security is enabled"
+  type        = bool
+  default     = false
+}
+
+variable "anonymous_auth_enabled" {
+  description = "Whether Anonymous auth is enabled. Enables fine-grained access control on an existing domain. Ignored unless advanced_security_options are enabled. Can only be enabled on an existing domain"
+  type        = bool
+  default     = false
+}
+
+variable "internal_user_database_enabled" {
+  description = "Whether the internal user database is enabled"
+  type        = bool
+  default     = false
+}
