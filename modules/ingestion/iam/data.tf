1a228cbdcf75ef323e0d21ea929da621617194fe
abhinavkumarsph <122246745+abhinavkumarsph@users.noreply.github.com>
2024-01-22 15:09:00 +0800

    restricted iam policy for ingestion pipeline (#26)

--- modules/ingestion/iam/data.tf
+++ modules/ingestion/iam/data.tf
@@ -7,8 +7,14 @@ data "aws_iam_policy_document" "opensearch_ingestion" {
   }
 
   statement {
-    effect    = "Allow"
-    actions   = ["es:ESHttp*"]
+    effect = "Allow"
+    actions = [
+      "es:ESHttpGet",
+      "es:ESHttpHead",
+      "es:ESHttpPatch",
+      "es:ESHttpPost",
+      "es:ESHttpPut",
+    ]
     resources = [for domain in var.opensearch_domain_arns : "${domain}/*"]
   }
 }
